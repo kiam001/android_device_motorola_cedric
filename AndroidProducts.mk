b2e12be0561c94113079e1fb8d677ff0e978a0a5
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-01 20:36:37 +0200

    change to bootleggersrom

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cos_cedric.mk
+    $(LOCAL_DIR)/bootleg_cedric.mk
 
 COMMON_LUNCH_CHOICES := \
-    cos_cedric-user \
-    cos_cedric-userdebug \
-    cos_cedric-eng
+    bootleg_cedric-user \
+    bootleg_cedric-userdebug \
+    bootleg_cedric-eng
