03b0f41d91caaf82c5e3ff0aefa1eaaf169d67d1
kiam001 <blablatupfen@gmail.com>
2019-10-13 13:14:06 +0200

    Update aosp_cedric.mk

--- aosp_cedric.mk
+++ aosp_cedric.mk
@@ -34,12 +34,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cedric-user 8.1.0 OPPS28.85-13-2 d04a4 release-keys"
-
 #Gapps
 TARGET_GAPPS_ARCH := arm64
 TARGET_MINIMAL_APPS := true
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/blueline/blueline:9/PQ3A.190801.002/5670241:user/release-keys
